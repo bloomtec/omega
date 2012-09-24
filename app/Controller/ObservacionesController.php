4a811a563ace2c633634f13aafbc5dfd10dfb001
Patrik Oskarsson <patrik.oskarsson@darpal.com>
2012-09-24 16:54:29 -0500

    response handler

--- app/Controller/ObservacionesController.php
+++ app/Controller/ObservacionesController.php
@@ -17,7 +17,7 @@ class ObservacionesController extends AppController {
 	 * las respuestas a observaciones.
 	 */
 	public function emailResponseHandler() {
-		$text = print_r($_POST, true);
+		$text = json_decode($_POST['mandrill_events']);
 		$this -> Observacion -> create();
 		$this -> Observacion -> save(
 			array(
@@ -26,7 +26,7 @@ class ObservacionesController extends AppController {
 					'modelo' => 'PruebasEnvio',
 					'llave_foranea' => 0,
 					'es_publico' => 0,
-					'texto' => $text
+					'texto' => debug($text)
 				)
 			)
 		);
